673eaa99e7780d9ab0e37c81583811e0233f4c14
Pedro Ha <ph@educreational.com>
2013-09-03 09:23:43 -0400

    Wrap EtsySearch in anonymous fn, trying different delayPerBatch but no success

--- web/js/etsy.js
+++ web/js/etsy.js
@@ -1,70 +1,126 @@
-var Etsy = {
-	// TODO: can refactor urls: base + app_id
-	listing_url: 		"https://openapi.etsy.com/v2/listings/active.js?api_key=skg82rpwr3x8t285zcj6z03w"
-  , product_url: 		"https://openapi.etsy.com/v2/listings/%id%.js?api_key=skg82rpwr3x8t285zcj6z03w"
-  , product_image_url:  "https://openapi.etsy.com/v2/listings/%id%/images.js?api_key=skg82rpwr3x8t285zcj6z03w"
+(function(exports) {
+	"use strict";
 
-  , getListing: function(callback, errback) {
+	var Etsy = {
+		// TODO: can refactor urls: base + app_id
+		listing_url: 		"https://openapi.etsy.com/v2/listings/active.js?api_key=skg82rpwr3x8t285zcj6z03w"
+	  , product_url: 		"https://openapi.etsy.com/v2/listings/%id%.js?api_key=skg82rpwr3x8t285zcj6z03w"
+	  , product_image_url:  "https://openapi.etsy.com/v2/listings/%id%/images.js?api_key=skg82rpwr3x8t285zcj6z03w"
 
-  		var deferred = $.Deferred();
-		deferred.then( callback, errback );
+	  , getListing: function(callback, errback) {
 
-		var getData = function(data) {
-			if (data.ok) {
-				deferred.resolve(data);
-			}
-			else {
-				var msg = "getListing(): Invalid response from server " + this.listing_url;
-				deferred.reject(msg);
-			}			
-		};
+	  		var deferred = $.Deferred();
+			deferred.then( callback, errback );
 
-		$.ajax({
-			url: this.listing_url
-		  , dataType: 'jsonp'
-		  , success: getData // cannot be part of the deferred
-		  , error: deferred.reject
-		});
-
-		return deferred;
-	}
-  , nop: function() {}
-  , getProductImages: function(product, errback) {
-  		var id = product.listing_id;
-  		var url = this.product_image_url.replace("%id%", id);
-		// alert(url);
-
-  		var deferred = $.Deferred();
-		deferred.then( function(product, images) {
-			product.images = images;
-		}, errback );
-
-		var getImageData = function(data) {
-			if (data.ok) {
-				if (data.count && data.count > 0) {
-					deferred.resolve(product, data.results);
+			var getData = function(data) {
+				if (data.ok) {
+					deferred.resolve(data);
 				}
 				else {
-					var msg = "getProductImages(): No images " + url;
+					var msg = "getListing(): Invalid response from server " + this.listing_url;
 					deferred.reject(msg);
-				}
-			}
-			else {
-				// Exceeded quote: 10 requests per second
-				var msg = "getProductImages(): Invalid response from server: " + JSON.stringify(data);
-				deferred.reject(msg);
-			}			
-		};
+				}			
+			};
 
-		$.ajax({
-			url: url
-		  , dataType: 'jsonp'
-		  , success: getImageData // cannot be part of the deferred
-		  , error: deferred.reject
+			$.ajax({
+				url: this.listing_url
+			  , dataType: 'jsonp'
+			  , success: getData // cannot be part of the deferred
+			  , error: deferred.reject
+			});
+
+			return deferred;
+		}
+	  , nop: function() {}
+	  , getProductImages: function(product, errback) {
+	  		var id = product.listing_id;
+	  		var url = this.product_image_url.replace("%id%", id);
+			// alert(url);
+
+	  		var deferred = $.Deferred();
+			deferred.then( function(product, images) {
+				product.images = images;
+			}, errback );
+
+			var getImageData = function(data) {
+				if (data.ok) {
+					if (data.count && data.count > 0) {
+						deferred.resolve(product, data.results);
+					}
+					else {
+						var msg = "getProductImages(): No images " + url;
+						deferred.reject(msg);
+					}
+				}
+				else {
+					// Exceeded quote: 10 requests per second
+					var msg = "getProductImages(): Invalid response from server: " + JSON.stringify(data);
+					deferred.reject(msg);
+				}			
+			};
+
+			$.ajax({
+				url: url
+			  , dataType: 'jsonp'
+			  , success: getImageData // cannot be part of the deferred
+			  , error: deferred.reject
+			});
+
+			return deferred;
+	  }	
+	};
+
+	var EtsySearch = (function() {
+
+		var errback = function(err) {
+			alert(err);
+		}
+
+		var listing = Etsy.getListing(function(){}, errback);
+
+		var imgLoadedDeferred = $.Deferred();
+
+		listing.done(function(data) {
+			var products = data.results;
+
+			var imgDeferreds = [];
+
+			// Delay every second: ETSY: up to 10 requests per second
+			var batches = products.length/8 + products.length%8;
+
+			var delayPerBatch = 5000; // A little over 1000 ms
+
+			for (var i = 0; i < batches; i++) {
+				(function(iter) {
+					setTimeout(function() {
+						var batch = products.slice(iter*8, 8);
+						for (var j = 0; j < batch.length; j++) {
+							var d = Etsy.getProductImages(batch[j], errback);
+							imgDeferreds.push(d);
+						}
+					}, delayPerBatch * iter);
+				})(i);
+			}
+
+			setTimeout(function() {
+				var collectiveDeferred = $.when.apply($, imgDeferreds);
+				collectiveDeferred.then( function() {
+					imgLoadedDeferred.resolve(products);
+				}, imgLoadedDeferred.reject );				
+			}, delayPerBatch * batches);
+
 		});
 
-		return deferred;
-  }	
-};
+		var find = function() {
+			return imgLoadedDeferred;
+		};
 
+		return {
+			find: find
+		}
+	})();
 
+	exports.Etsy = Etsy;
+	exports.EtsySearch = EtsySearch;
+
+})(window);
