9dbeef156d0b56f2717ec547f2f0a890839f6fed
Pedro Ha <ph@educreational.com>
2013-09-03 01:19:04 -0400

    Refactoring for using

--- web/js/etsy.js
+++ web/js/etsy.js
@@ -2,24 +2,25 @@ var Etsy = {
 	// TODO: can refactor urls: base + app_id
 	listing_url: 		"https://openapi.etsy.com/v2/listings/active.js?api_key=skg82rpwr3x8t285zcj6z03w"
   , product_url: 		"https://openapi.etsy.com/v2/listings/%id%?api_key=skg82rpwr3x8t285zcj6z03w"
-  , product_image_url: "https://openapi.etsy.com/v2/listings/%id%/images?api_key=skg82rpwr3x8t285zcj6z03w"
+  , product_image_url:  "https://openapi.etsy.com/v2/listings/%id%/images?api_key=skg82rpwr3x8t285zcj6z03w"
 
-  , getListing: function(callback, errback) {
+  , getListing: function(deferred) {
 		
 		var getData = function(data) {
 			if (data.ok) {
-				callback(data);
+				deferred.resolve(data);
 			}
 			else {
-				errback("getListing(): Invalid response from server " + this.listing_url);
+				var msg = "getListing(): Invalid response from server " + this.listing_url;
+				deferred.reject(msg);
 			}			
 		};
 
-		var deferred = $.ajax({
+		$.ajax({
 			url: this.listing_url
 		  , dataType: 'jsonp'
 		  , success: getData // cannot be part of the deferred
-		  , error: errback
+		  , error: deferred.reject
 		});
 	}
 };
