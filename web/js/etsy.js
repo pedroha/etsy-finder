b5f7a4fc845b2887be83bb005e9957aa09cd63ff
Pedro Ha <ph@educreational.com>
2013-09-03 08:27:10 -0400

    Add .js in URL for etsy_get_images.

--- web/js/etsy.js
+++ web/js/etsy.js
@@ -1,11 +1,14 @@
 var Etsy = {
 	// TODO: can refactor urls: base + app_id
 	listing_url: 		"https://openapi.etsy.com/v2/listings/active.js?api_key=skg82rpwr3x8t285zcj6z03w"
-  , product_url: 		"https://openapi.etsy.com/v2/listings/%id%?api_key=skg82rpwr3x8t285zcj6z03w"
-  , product_image_url:  "https://openapi.etsy.com/v2/listings/%id%/images?api_key=skg82rpwr3x8t285zcj6z03w"
+  , product_url: 		"https://openapi.etsy.com/v2/listings/%id%.js?api_key=skg82rpwr3x8t285zcj6z03w"
+  , product_image_url:  "https://openapi.etsy.com/v2/listings/%id%/images.js?api_key=skg82rpwr3x8t285zcj6z03w"
 
-  , getListing: function(deferred) {
-		
+  , getListing: function(callback, errback) {
+
+  		var deferred = $.Deferred();
+		deferred.then( callback, errback );
+
 		var getData = function(data) {
 			if (data.ok) {
 				deferred.resolve(data);
@@ -22,5 +25,44 @@ var Etsy = {
 		  , success: getData // cannot be part of the deferred
 		  , error: deferred.reject
 		});
+
+		return deferred;
 	}
+  , nop: function() {}
+  , getProductImages: function(product, errback) {
+  		var id = product.listing_id;
+  		var url = this.product_image_url.replace("%id%", id);
+		// alert(url);
+
+  		var deferred = $.Deferred();
+		deferred.then( function(product, images) {
+			product.images = images;
+		}, errback );
+
+		var getImageData = function(data) {
+			if (data.ok) {
+				if (data.count && data.count > 0) {
+					deferred.resolve(product, data.results);
+				}
+				else {
+					var msg = "getProductImages(): No images " + url;
+					deferred.reject(msg);
+				}
+			}
+			else {
+				// Exceeded quote: 10 requests per second
+				var msg = "getProductImages(): Invalid response from server: " + JSON.stringify(data);
+				deferred.reject(msg);
+			}			
+		};
+
+		$.ajax({
+			url: url
+		  , dataType: 'jsonp'
+		  , success: getImageData // cannot be part of the deferred
+		  , error: deferred.reject
+		});
+
+		return deferred;
+  }	
 };
