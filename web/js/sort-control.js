2b07d5d8288ab41889a897865a91b27ff8046684
Pedro Ha <ph@educreational.com>
2013-09-09 16:10:52 -0400

    Refactoring JS in 09 to sort-control.js

--- web/js/sort-control.js
+++ web/js/sort-control.js
@@ -1,47 +1,35 @@
 var ProductRouter = Backbone.Router.extend({
+	initialize: function(listing) {
+		this.listing = listing;
+	}
+  , routes: {
+		"": "empty",
+		"about" : "showAbout",
+		"listing/:keywords": "search",
+		"listing/:keywords/:sort_field/:sort_dir" : "search",
+		"listing/:keywords/:sort_field/:sort_dir/:id" : "search"
+	}
+  , empty: function() {
+		//alert("empty root");
+		this.search();
+	}
+  , showAbout: function() { console.log("hello About"); }
+  , search: function(keywords, sort_field, sort_dir, id) {
+		// Client-side sorting
 
-  routes: {
-  	"": "empty",
-    "about" : "showAbout",
-    "listing/:keywords": "search",
-    "listing/:keywords/:sort_field/:sort_dir" : "search",
-    "listing/:keywords/:sort_field/:sort_dir/:id" : "search"
-  },
+		keywords = keywords || "";
+		sort_field = sort_field || "price";
+		sort_dir = sort_dir || "DESC";
 
-  empty: function() {
-  	//alert("empty root");
-  	this.search();
-  },
-
-  showAbout: function() { console.log("hello About"); },
-
-  search: function(keywords, sort_field, sort_dir, id) {
-
-  	// Better search: everytime we SORT by field -> query again! but more expensive!?
-
-  	keywords = keywords || "";
-  	sort_field = sort_field || "price";
-  	sort_dir = sort_dir || "DESC";
-
-  	/*
-  	if (id) {
-  		alert("We need to show Product " + id);
-  	}
-  	else {
-  		alert("Showing... " + keywords + " " + sort_field + " / " + sort_dir);
-  	}
-  	*/
-
-  	// SORT Dilemma: several options
-  	// 1) Requery with new sort options back to the server (better data with sorting)
-	// 2) Sort only on the client data (with Backbone sort)
-	// 3) Use isotope for visual sorting purely on the client data
-
-	// For simplicity and accuracy, 1) requery the server?
-
-  }
+		if (id) {
+			alert("We need to show Product " + id);
+		}
+		else {
+			this.listing.collection.setSortField(sort_field, sort_dir);
+			this.listing.render();
+		}
+	}
 });
-
 
 var SortControl = (function() {
 
@@ -65,6 +53,7 @@ var SortControl = (function() {
 		sortOptions.direction = "ASC";
 		showSort(sortOptions);
 	});
+
 	$('#sort-desc-btn').on('click', function() {
 		sortOptions.direction = "DESC";
 		showSort(sortOptions);
