33a02fe9bf7e275eea1b90db23ee0cae470d360d
Pedro Ha <ph@educreational.com>
2014-07-31 17:52:23 -0400

    Use strict for js modules

--- web/js/search-control.js
+++ web/js/search-control.js
@@ -1,55 +1,61 @@
-var SearchControl = {
-	init: function(listing) {
-		this.listing = listing;
+(function(exports, $) {
 
-		var self = this;
+	var SearchControl = {
+		init: function(listing) {
+			this.listing = listing;
 
-		var search = function(evt) {
-			var keywords = self.getKeywords();					
-			SearchControl.searchByKeywords(keywords);
-			return false;
-		};
+			var self = this;
 
-		$('#search-btn').on('click', search);
-		$('#keywords').on('keypress', function(evt) {
-			if (evt.keyCode === 13) {
-				return search(evt);
-			}
-		});
-	}
-  , getKeywords: function() {
-		var keywords = $('#keywords').val();
-		return keywords;  	
-  }
-  , searchByKeywords: function(keywords) {
-  		var options = null;
+			var search = function(evt) {
+				var keywords = self.getKeywords();					
+				SearchControl.searchByKeywords(keywords);
+				return false;
+			};
 
-  		if (keywords) {
-  			options = {
-  				keywords: keywords
-  			}
-  		}
+			$('#search-btn').on('click', search);
+			$('#keywords').on('keypress', function(evt) {
+				if (evt.keyCode === 13) {
+					return search(evt);
+				}
+			});
+		}
+	  , getKeywords: function() {
+			var keywords = $('#keywords').val();
+			return keywords;  	
+	  }
+	  , searchByKeywords: function(keywords) {
+	  		var options = null;
 
-  		$.blockUI({
-  			message: 'Loading content &nbsp;&nbsp;&nbsp;<img src="img/ajax/clover_48.gif" style="position:relative;top:10px;left:0"/>',
-  			css: {
-  				font: '2em Georgia',
-  				'line-height': '2em',
-  				'vertical-text-align': 'text-top',
-	            border: 'none', 
-	            padding: '15px', 
-	            backgroundColor: '#000', 
-	            '-webkit-border-radius': '10px', 
-	            '-moz-border-radius': '10px', 
-	            opacity: .5, 
-	            color: '#fff'
-	        }
-	    });
-	    setTimeout($.unblockUI, 10000); // MAX timeout: 10 seconds
+	  		if (keywords) {
+	  			options = {
+	  				keywords: keywords
+	  			}
+	  		}
 
-	    var deferred = this.listing.collection.fetch(options);
-		deferred.done(function(products) {
-			$.unblockUI();
-		});
- 	}
-};
+	  		$.blockUI({
+	  			message: 'Loading content &nbsp;&nbsp;&nbsp;<img src="img/ajax/clover_48.gif" style="position:relative;top:10px;left:0"/>',
+	  			css: {
+	  				font: '2em Georgia',
+	  				'line-height': '2em',
+	  				'vertical-text-align': 'text-top',
+		            border: 'none', 
+		            padding: '15px', 
+		            backgroundColor: '#000', 
+		            '-webkit-border-radius': '10px', 
+		            '-moz-border-radius': '10px', 
+		            opacity: .5, 
+		            color: '#fff'
+		        }
+		    });
+		    setTimeout($.unblockUI, 10000); // MAX timeout: 10 seconds
+
+		    var deferred = this.listing.collection.fetch(options);
+			deferred.done(function(products) {
+				$.unblockUI();
+			});
+	 	}
+	};
+
+	exports.SearchControl = SearchControl;
+
+})(window, jQuery);
--- web/js/sort-control.js
+++ web/js/sort-control.js
@@ -1,70 +1,79 @@
-var ProductRouter = Backbone.Router.extend({
-	initialize: function(listing) {
-		this.listing = listing;
-	}
-  , routes: {
-		"": "empty",
-		"about" : "showAbout",
-		"listing/:keywords": "search",
-		"listing/:keywords/:sort_field/:sort_dir" : "search",
-		"listing/:keywords/:sort_field/:sort_dir/:id" : "search"
-	}
-  , empty: function() {
-		//alert("empty root");
-		this.search();
-	}
-  , showAbout: function() { console.log("hello About"); }
-  , search: function(keywords, sort_field, sort_dir, id) {
-		// Client-side sorting
+(function(exports, $, Backbone, _) {
+	"use strict";
 
-		keywords = keywords || "";
-		sort_field = sort_field || "price";
-		sort_dir = sort_dir || "DESC";
-
-		if (id) {
-			alert("We need to show Product " + id);
+	var ProductRouter = Backbone.Router.extend({
+		initialize: function(listing) {
+			this.listing = listing;
 		}
-		else {
-			this.listing.collection.setSortField(sort_field, sort_dir);
-			this.listing.render();
+	  , routes: {
+			"": "empty",
+			"about" : "showAbout",
+			"listing/:keywords": "search",
+			"listing/:keywords/:sort_field/:sort_dir" : "search",
+			"listing/:keywords/:sort_field/:sort_dir/:id" : "search"
 		}
-	}
-});
+	  , empty: function() {
+			//alert("empty root");
+			this.search();
+		}
+	  , showAbout: function() { console.log("hello About"); }
+	  , search: function(keywords, sort_field, sort_dir, id) {
+			// Client-side sorting
 
-var SortControl = (function() {
+			keywords = keywords || "";
+			sort_field = sort_field || "price";
+			sort_dir = sort_dir || "DESC";
 
-	var showSort = function(options) {
-		console.log(options);
-	};
-
-	var init = function(fn) {
-		showSort = fn;
-	};
-
-	// Sort control		
-	var sortOptions = {
-		direction: "ASC"
-	  , field: "title"
-	};
-	
-	$('#sort-field-select').val(sortOptions.field);
-
-	$('#sort-asc-btn').on('click', function() {
-		sortOptions.direction = "ASC";
-		showSort(sortOptions);
+			if (id) {
+				alert("We need to show Product " + id);
+			}
+			else {
+				this.listing.collection.setSortField(sort_field, sort_dir);
+				this.listing.render();
+			}
+		}
 	});
 
-	$('#sort-desc-btn').on('click', function() {
-		sortOptions.direction = "DESC";
-		showSort(sortOptions);
-	});
+	var SortControl = (function() {
 
-	$('#sort-field-select').on('change', function() {
-		sortOptions.field = $(this).val();
-		showSort(sortOptions);
-	});
+		var showSort = function(options) {
+			console.log(options);
+		};
 
-	return {
-		init: init
-	};
-})();
+		var init = function(fn) {
+			showSort = fn;
+		};
+
+		// Sort control		
+		var sortOptions = {
+			direction: "ASC"
+		  , field: "title"
+		};
+		
+		$('#sort-field-select').val(sortOptions.field);
+
+		$('#sort-asc-btn').on('click', function() {
+			sortOptions.direction = "ASC";
+			showSort(sortOptions);
+		});
+
+		$('#sort-desc-btn').on('click', function() {
+			sortOptions.direction = "DESC";
+			showSort(sortOptions);
+		});
+
+		$('#sort-field-select').on('change', function() {
+			sortOptions.field = $(this).val();
+			showSort(sortOptions);
+		});
+
+		return {
+			init: init
+		};
+	})();
+
+	exports.ProductRouter = ProductRouter;
+	exports.SortControl = SortControl;
+
+})(window, jQuery, Backbone, _);
+
